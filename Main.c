a849bec5429493e64805d1d95faae7854cb5af17
vardie95 <vardie95@gmail.com>
2018-03-09 23:14:54 -0600

    add

---
+++ Main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int **matrix = NULL;
+
+int main(int argc, char *argv[] ){
+	if (argc == 1) {
+		printf("un argumento");
+
+
+
+	}else {
+
+		printf("Error" );
+	}
+	/*int habitantesVivienda[5][25];
+	habitantesVivienda[3][24] = 4;
+	printf("El numero de personas que viven en la vivienda 24 del piso 3 es %d\n", habitantesVivienda[3][24]);*/
+
+	return 0; 
+	
+}
