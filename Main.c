c206842815d2e375cb69cd62d4d8bf8ea5e21d56
Charlie <charlie.tec.vm@gmail.com>
2018-03-13 18:53:45 -0600

    Modo cientifico list Haga algunas pruebillas para ver si esta bien.

--- Main.c
+++ Main.c
@@ -14,32 +14,50 @@ int main(int argc, char *argv[] ){
 	srand(time(NULL));
 	
 	if (argc == 2) {
-		if( ( !(strcmp(argv[1], "x")))) {
+		if( ( !(strcmp(argv[1], "x")))) { // If user passes the value x - Experimental mode starts
 			printf("Modo Experimental\n");
-			int items_value[6];
-			int items_size[6];
-			int items_value_2[6];
-			int items_size_2[6];
+
+			int items_value[6];     // Variables values for basic greedy 
+			int items_size[6];      // Variables sizes for basic greedy
+
+			int items_value_2[6];   // Variables values for proportional greedy
+			int items_size_2[6];    // Variables size for proportional greedy
+
+			int items_value_3[6];   // Variables values for dinamic programming solution
+			int items_size_3[6];    // Variables size for dinamic programming solution
+
 			for (int i = 0; i < 6; i++) {
-				int rand_value = (rand() % 20) + 1;
-				int rand_size = (rand() % 7) + 1;
+				int rand_value = (rand() % 20) + 1; // Variable value randomizer
+				int rand_size = (rand() % 7) + 1;   // Variable size randomizer
 				items_value[i] = rand_value;
 				items_size[i] = rand_size;
 				items_value_2[i] = rand_value;
 				items_size_2[i] = rand_size;
-				printf("%d, %d\n", items_value[i], items_size[i]);
+				items_value_3[i] = rand_value;
+				items_size_3[i] = rand_size;
+				printf("%s%d%s%d%s%d\n", "Item ", i, " - Value: ", items_value[i], " / Size: ", items_size[i]);
 			}
-			printf("%d\n", sizeof(items_value) / sizeof(int));
-			basicGreedy(15, items_value, items_size);
+
+			printf("%s\n", "");
+
+			basicGreedy(15, items_value, items_size, 6); // The basic greedy algorithm is execute
+
 			printf("\n%s\n", "------------------------------------------");
-			proportionalGreedy(15, items_value_2, items_size_2);
-		}else if ( ( !(strcmp(argv[1], "y")))){
+
+			proportionalGreedy(15, items_value_2, items_size_2, 6); // The proportional greedy algorithm is execute
+
+			printf("\n%s\n", "------------------------------------------");
+
+			dinamicProgramming(16, 6, items_value_3, items_size_3); // The dinamic programming algorithm is execute (We pass a value of 16 because we need 0 to 15 rows)
+
+
+		} else if ( ( !(strcmp(argv[1], "y")))){
 			printf("Modo  Cientifico\n");
-			dinamicExample();
-		}else{
+			//dinamicExample();
+		} else{
 			printf("Error:Give a correct argument\n");
 		}
-	}else {
+	} else {
 		printf("Error: Please provide the correct argument\n" );
 	}
 	return 0; 
@@ -79,27 +97,26 @@ void makeBeamer(){
 
 }
 
-void basicGreedy(int bag_size, int items_value[], int items_size[]){
+void basicGreedy(int bag_size, int items_value[], int items_size[], int len){ // Basic greedy algorithm
+	printf("%s\n", "Basic Greedy:");
 	makeBeamer();
 	int bag_value = 0;
 	int temp_higher_value = 0;
 	int temp_size = 0;
 	int temp_index;	
 	bool space_available = true;
-	int n = sizeof(items_value) / sizeof(int);
 	while (space_available) {
 		space_available = false;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < len; i++) {
 			if (items_value[i] > temp_higher_value && items_size[i] <= bag_size) {
 				temp_higher_value = items_value[i];
 				temp_size = items_size[i];
 				temp_index = i;
 				space_available = true;
-				break;
 			}
 		}
 		if (space_available == true) {
-			printf("%s%d%s%d\n", "Item ", temp_index, ": was added to the bag.BAG: ", bag_size);
+			printf("%s%d%s%d%s%d\n", "Item ", temp_index, ": was added to the bag.BAG: ", bag_size, " -> ", bag_size - temp_size);
 			bag_value += temp_higher_value;
 			bag_size -= temp_size;
 			items_value[temp_index] = 0;
@@ -108,32 +125,27 @@ void basicGreedy(int bag_size, int items_value[], int items_size[]){
 	}
 }
 
-void proportionalGreedy(int bag_size, int items_value[], int items_size[]) {
+void proportionalGreedy(int bag_size, int items_value[], int items_size[], int len) {
+	printf("%s\n", "Proportional Greedy:");
 	int bag_value = 0;
 	float temp_higher_proportion = 0;
 	int temp_size = 0;
 	int temp_index;	
-	int n = sizeof(items_value) / sizeof(int);
 	bool space_available = true;
-	for (int i = 0; i < 6; i++) {
-		printf("%s%d%s%d%s%d\n", "Item ", i, " - Value: ", items_value[i], " / Size: ", items_size[i]);
-	}
-	printf("%s\n", "");
 	while (space_available) {
 		space_available = false;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < len; i++) {
 			if (items_value[i] > 0){
 				if ((items_value[i] / items_size[i]) > temp_higher_proportion && items_size[i] <= bag_size) {
 					temp_higher_proportion = items_value[i] / items_size[i];
 					temp_size = items_size[i];
 					temp_index = i;
 					space_available = true;
-					break;
 				}
 			}
 		}
 		if (space_available == true) {
-			printf("%s%d%s\n", "Item ", temp_index, ": was added to the bag.");
+			printf("%s%d%s%d%s%d\n", "Item ", temp_index, ": was added to the bag.BAG: ", bag_size, " -> ", bag_size - temp_size);
 			bag_value += items_value[temp_index];
 			bag_size -= temp_size;
 			items_value[temp_index] = 0;
@@ -142,25 +154,9 @@ void proportionalGreedy(int bag_size, int items_value[], int items_size[]) {
 	}
 }
 
-void dinamicExample() {
+void dinamicProgramming(int m, int n, int items_value[], int items_size[]) {
+	printf("%s\n", "Dinamic Programming:");
 	srand(time(NULL));
-	int items_value[7];
-	int items_size[7];
-	items_value[0] = 7;
-	items_value[1] = 9;
-	items_value[2] = 5;
-	items_value[3] = 12;
-	items_value[4] = 14;
-	items_value[5] = 6;
-	items_value[6] = 12;
-	items_size[0] = 3;
-	items_size[1] = 4;
-	items_size[2] = 2;
-	items_size[3] = 6;
-	items_size[4] = 7;
-	items_size[5] = 3;
-	items_size[6] = 5;
-
 	/*for (int i = 0; i < 10; i++) {
 		items_value[i] = (rand() % 100) + 1;
 		items_size[i] = (rand() % 40) + 1;
@@ -168,21 +164,12 @@ void dinamicExample() {
 	}*/
 	int **p;
   	int i, j;
-  	int m = 16;
-  	int n = 7;
   	p = malloc(sizeof(float *) * m); /* Row pointers */
 	for(i = 0; i < m; i++) {
 		p[i] =  malloc(sizeof(float) * n); /* Rows */
 	}
 
-	/* Assign values to array elements and print them */
-  	/*for(i = 0; i < m; i++) {
-	    for(j = 0; j < n; j++) {
-	      p[i][j] = (i * 10) + (j + 1);
-	      printf("%6d",p[i][j]);
-	    }
-	    printf("\n");
-    }*/
+	/* Assign values to array elements */
     for (j = 0; j < n; j++) {
     	for (i = 0; i < m; i++) {
     		if (items_size[j] <= i) {
